c8482da18f70c8f5c593e4e9ff960c09e9f7340d
Mark <markchristison@gmail.com>
2022-08-19 16:03:32 +1200

    pangram soloution

---
+++ pangram/pangram.ts
@@ -0,0 +1,6 @@
+export function isPangram(sentance: string): boolean {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const lowerCaseSentence = sentance.toLowerCase()
+    return alphabet.split('').every(letter => lowerCaseSentence.includes(letter))
+
+}
